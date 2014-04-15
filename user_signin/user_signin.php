1d71be2a0c26e2c050b17a62b49210a09064e029
shibuya246 <blog@shibuya246.com>
2014-04-16 01:30:43 +0900

    typo
    http://forums.hotarucms.org/threads/user-signin-plugin-update.6962/

--- user_signin/user_signin.php
+++ user_signin/user_signin.php
@@ -633,7 +633,7 @@ class UserSignin
             return true;
         }
         
-        $h->pluginHook('user_signin_register_check_blocked');  // Stop Spam is one plugin that uses this
+        $h->pluginHook('users_signin_register_check_blocked');  // Stop Spam is one plugin that uses this
         if (isset($h->vars['block']) && $h->vars['block'] == true) { return true; }
 
         return false;   // not blocked
